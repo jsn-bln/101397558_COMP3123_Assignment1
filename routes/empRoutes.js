f68e153df7ebe93de7f88379c066831160428b95
jsn-bln <jaysonbalano99@gmail.com>
2023-10-11 20:34:49 -0400

    finished update and delete employee route

--- routes/empRoutes.js
+++ routes/empRoutes.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const Employee = require('../model/EmployeeSchema');
 const mongoose = require('mongoose');
 
+
+
+
+const isGenderValid = (gender) => {
+    return ['male', 'female', 'other'].includes(gender.toLowerCase());
+  };
+
+
+
 //ENDPOINT 1
 // http://localhost:8080/api/v1/emp/employees
 
@@ -23,8 +32,7 @@ router.post('/employees', (req, res) => {
     const {firstname, lastname, email, gender, salary} = req.body
 
     //validate gender
-    if(gender.toLowerCase() !== "male" && gender.toLowerCase() !== "female"
-        && gender.toLowerCase() !== "other") {
+    if(!isGenderValid(gender)) {
         res.json({"message" : "gender field must be male/female/other"})
     }
     else{
@@ -32,7 +40,7 @@ router.post('/employees', (req, res) => {
             firstname,
             lastname,
             email,
-            gender,
+            gender: gender.toLowerCase(),
             salary
         })
     
@@ -46,6 +54,76 @@ router.post('/employees', (req, res) => {
                 res.status(500).json({ error: 'Employee failed to be added.' });
             }); 
     }
+})
+
+//ENDPOINT 3
+//http://localhost:8080/api/v1/emp/employees/{eid}
+
+router.get('/employees/:eid', (req, res) => {
+    const eid = req.params.eid;
+    Employee.findOne({_id: eid})
+        .then((user) => {
+            res.status(200).json({user})
+        })
+        .catch((err) => {
+            res.json({err})
+        })
+})
+
+
+//ENDPOINT 4
+//http://localhost:8080/api/v1/emp/employees/{eid}
+
+router.put('/employees/:eid', (req, res) => {
+    const eid = req.params.eid;
+    Employee.findOne({_id: eid})
+        .then((user) => {
+            // Checks if theres a values passed in body
+            // if no values are found then will retain the original value
+            user.firstname = req.body.firstname || user.firstname;
+            user.lastname = req.body.lastname || user.lastname;
+            user.email = req.body.email || user.email;
+            user.salary = req.body.salary || user.salary;
+            
+            // validate gender
+            if(req.body.gender != null){
+                if(isGenderValid(req.body.gender)){
+                    user.gender = req.body.gender.toLowerCase();
+                }
+            }
+                
+                
+
+            user.save()
+                .then(() => {
+                    if(!isGenderValid(req.body.gender) && req.body.gender !== null)
+                    {
+                        res.status(400).json({'message' : 'invalid gender type, gender wasnt updated!'})
+                    }
+                    else{
+                        res.status(200).json({'message' : 'Employee has been updated!'})
+                    }
+                })
+                .catch((err) => {
+                    res.json({err})
+                })
+        })
+        .catch((err) => {
+            res.json({err})
+        })
+})
+
+
+router.delete('/employees', (req, res) => {
+    const eid = req.query.eid
+
+    Employee.deleteOne({_id: eid})
+        .then(() => {
+            res.status(204).json({'message' : 'Employee deleted successfully!'})
+        })
+        .catch((err) => {
+            res.json({'message' : 'Employee doesnt exist!'})
+        })
 })
 
 
